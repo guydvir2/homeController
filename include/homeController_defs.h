ecb1e0b0bf604f64b29c219b825fc52476a75830
guyd <guydvir2@gmail.com>
2023-05-22 18:25:03 +0300

    update debug

--- include/homeController_defs.h
+++ include/homeController_defs.h
@@ -4,13 +4,21 @@
 
 struct Cotroller_Ent_telemetry
 {
-    uint8_t id;           /* of entity instance*/
-    uint8_t type;         /* Entiry type 0- win. 1 sw */
-    uint8_t state;        /* Up/Down/ Off */
-    uint8_t trig;         /* What triggered the button */
-    float position = 200; /* For windows only */
-    bool newMSG = false;  /* NewMSG for loop function */
-    unsigned long timeout = 0;
+    // Common fields //
+    uint8_t id;                  /* of entity instance*/
+    uint8_t type;                /* Entiry type 0- win. 1 sw */
+    uint8_t state;               /* Up/Down/ Off */
+    uint8_t trig;                /* What triggered the button */
+    bool newMSG = false;         /* NewMSG for loop function */
+    bool lockdown_state = false; /* For windows only */
+
+    // Switch only fields //
+    uint8_t pwm = 255;         /* PWM % */
+    uint8_t pressCount = 0;    // multi_press switch
+    unsigned long timeout = 0; // Timeout end time
+
+    // Window only fields //
+    float position = 200; /* Open position 0-100 */
 };
 enum ENT_TYPE : const uint8_t
 {
